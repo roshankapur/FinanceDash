1760ed2e61272683b0e6209c77512c60c4ba86eb
Roshan Kapur <kapurroshan2003@gmail.com>
2024-01-15 10:21:14 +0000

    created navbar and setting up page layouts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,8 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
-import Navbar from "@/scenes/Navbar";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function  App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -17,7 +18,7 @@ function  App() {
           <Box width = "100%" height = "100%" padding = "1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard/>} />
               <Route path="/predictions" element={<div>predictions page</div>} />
             </Routes>
           </Box>
