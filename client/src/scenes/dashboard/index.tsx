85271e70edd13399a1a7869c002e57be0eb74d01
Roshan Kapur <kapurroshan2003@gmail.com>
2024-01-24 00:35:47 +0000

    initialised api, refined dashboard layout and created subcomponents per row to make it more manageable to add features to each widget, initialised backend and installed packages

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,4 +1,7 @@
-import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 type Props = {}
 
@@ -46,8 +49,7 @@ const gridTemplateSmallScreens = `
 
 `;
 
-const Dashboard = (props: Props) => {
-    const { palette } = useTheme();
+const Dashboard = () => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
     return (
         <Box 
@@ -64,17 +66,10 @@ const Dashboard = (props: Props) => {
                     gridAutoColumns: "1fr",
                     gridAutoRows: "80px",
                     gridTemplateAreas: gridTemplateSmallScreens,
-                }}>
-                <Box bgcolor="#fff" gridArea="a"></Box>
-                <Box bgcolor="#fff" gridArea="b"></Box>
-                <Box bgcolor="#fff" gridArea="c"></Box>
-                <Box bgcolor="#fff" gridArea="d"></Box>
-                <Box bgcolor="#fff" gridArea="e"></Box>
-                <Box bgcolor="#fff" gridArea="f"></Box>
-                <Box bgcolor="#fff" gridArea="g"></Box>
-                <Box bgcolor="#fff" gridArea="h"></Box>
-                <Box bgcolor="#fff" gridArea="i"></Box>
-                <Box bgcolor="#fff" gridArea="j"></Box>
+            }}>
+                <Row1/>
+                <Row2/>
+                <Row3/>
 
         </Box>
     );
