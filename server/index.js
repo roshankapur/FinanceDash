7868df8db3120d34e4679f321849f91c488fa7f4
Roshan Kapur <kapurroshan2003@gmail.com>
2024-01-24 07:31:34 +0000

    still setting up backend server, mongoDB not connecting to application despite correct uri

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express  from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/* CONFIG */
+dotenv.config()
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(async() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+}).catch((error) => console.log(`${error} did not connect`));
